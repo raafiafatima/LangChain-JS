372484ad9cff7b7ce0840726829ae0cff363fb4f
Raafia Fatima <raafia.fatima.2004@gmail.com>
2025-08-15 19:33:16 +0500

    RAG Chatbot

--- Tutorial/RAG-ChatBot/index.js
+++ Tutorial/RAG-ChatBot/index.js
@@ -1,7 +1,38 @@
-document.addEventListener('submit', (e) => {
-    e.preventDefault()
-    progressConversation()
+import dotenv from "dotenv";
+dotenv.config();
+import {ChatGoogleGenerativeAI} from '@langchain/google-genai'
+import { PromptTemplate } from "@langchain/core/prompts";
+
+
+// document.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     progressConversation()
+// })
+
+const googleAPIkey = process.env.GOOGLR_API_KEY
+const llm = new ChatGoogleGenerativeAI({
+     model: "gemini-2.0-flash",
+    apiKey : googleAPIkey
 })
+
+// adding a retriver, that gets the closet match according to the semantic value of the question 
+
+// create embeddings and 
+
+const instruction = 'Given a question, turn it into a standalone question. question : {question} standalone question :' 
+// minimal instruction for what the llm has to do 
+const chatPrompt = PromptTemplate.fromTemplate(instruction)
+// returns a string that contains the variables filled in, such as the question variable would have the value filled in 
+const ChatChain = chatPrompt.pipe(llm)
+// pipe is a function that connects two objects, taking the output of one and entering it as an input in the second - here it takes the string with input from the prompt and then passes it onto the llm for response 
+const response = await ChatChain.invoke({
+    question : 'I am a student at Karachi and want to take admission in a good engineering university, i am unable to find the whole admissions process, can you describe it to me step by step'
+})
+// this gives us the standalone question - a simple question that only contains the thing that the user needs 
+
+
+
+console.log(response) 
 
 function progressConversation() {
     const userInput = document.getElementById('user-input')
