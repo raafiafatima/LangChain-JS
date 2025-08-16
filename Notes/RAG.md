4f19bd0653e5fa46162e4bb80d41675afe6c78c0
Raafia Fatima <raafia.fatima.2004@gmail.com>
2025-08-16 10:59:58 +0500

    feat: RAG Chatbot added

--- Notes/RAG.md
+++ Notes/RAG.md
@@ -6,3 +6,7 @@
 4. After making the store, we create embeddings, mostly use OpenAI, but cause i am broke, i will be using Google Generative Al. 
 5. After creating the embeddings, we create a supabase client and then add the embeddings into the supabase vector database. 
 6. some common errors that were faced - making sure the dimensions for the vector resonate with the modal
+
+7. we now start the process of making a standalone question using the users input and then finding appropriate chunks for it in the vector store. the only purpose for the standalone question is to get relevant chunks, the original input is passed onto the LLM 
+8. the standalone question si made using a PromptTemplate that gives instructions to the llms on what output they need to produce 
+9. 
--- Tutorial/RAG-ChatBot/index.js
+++ Tutorial/RAG-ChatBot/index.js
@@ -1,56 +1,66 @@
 import dotenv from "dotenv";
 dotenv.config();
-import {ChatGoogleGenerativeAI} from '@langchain/google-genai'
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { PromptTemplate } from "@langchain/core/prompts";
-
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import  retriver  from "./utils/retriver.js";
 
 // document.addEventListener('submit', (e) => {
 //     e.preventDefault()
 //     progressConversation()
 // })
 
-const googleAPIkey = process.env.GOOGLR_API_KEY
+const googleAPIkey = process.env.GOOGLE_API_KEY;
 const llm = new ChatGoogleGenerativeAI({
-     model: "gemini-2.0-flash",
-    apiKey : googleAPIkey
-})
+  model: "gemini-2.0-flash",
+  apiKey: googleAPIkey,
+});
 
-// adding a retriver, that gets the closet match according to the semantic value of the question 
+// for getting standalone question: 
+const instruction =
+  "Given a question, turn it into a standalone question. question : {question} standalone question :";
+// minimal instruction for what the llm has to do
+const chatPrompt = PromptTemplate.fromTemplate(instruction);
+// returns a string that contains the variables filled in, such as the question variable would have the value filled in
 
-// create embeddings and 
+// for getting chat response 
+const answerInst = 'You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really dont know the answer, say "Im sorry, I dont know the answer to that." And direct the questioner to email help@scrimba.com. Dont try to make up an answer. Always speak as if you were chatting to a friend context: {context} question: {question} answer:'
+const answerPrompt = PromptTemplate.fromTemplate(answerInst)
 
-const instruction = 'Given a question, turn it into a standalone question. question : {question} standalone question :' 
-// minimal instruction for what the llm has to do 
-const chatPrompt = PromptTemplate.fromTemplate(instruction)
-// returns a string that contains the variables filled in, such as the question variable would have the value filled in 
-const ChatChain = chatPrompt.pipe(llm)
-// pipe is a function that connects two objects, taking the output of one and entering it as an input in the second - here it takes the string with input from the prompt and then passes it onto the llm for response 
+const ChatChain = chatPrompt
+  .pipe(llm)
+  .pipe(new StringOutputParser())
+  .pipe(retriver);
+//here we need to pass along the string of content from ll. and not the whole object. we can use dot notation, or use an output parser that automatically converts the data into the specified type.
+// pipe is a function that connects two objects, taking the output of one and entering it as an input in the second - here it takes the string with input from the prompt and then passes it onto the llm for response
+
 const response = await ChatChain.invoke({
-    question : 'I am a student at Karachi and want to take admission in a good engineering university, i am unable to find the whole admissions process, can you describe it to me step by step'
-})
-// this gives us the standalone question - a simple question that only contains the thing that the user needs 
+  question:
+    "I am a student at Karachi and want to take admission in a good engineering university, i am unable to find the whole admissions process, can you describe it to me step by step",
+});
+// this gives us the standalone question - a simple question that only contains the thing that the user needs
 
-
-
-console.log(response) 
+console.log(response); // returns the final 4 chunks of relevant data needed from the vector store
 
 function progressConversation() {
-    const userInput = document.getElementById('user-input')
-    const chatbotConversation = document.getElementById('chatbot-conversation-container')
-    const question = userInput.value
-    userInput.value = ''
+  const userInput = document.getElementById("user-input");
+  const chatbotConversation = document.getElementById(
+    "chatbot-conversation-container"
+  );
+  const question = userInput.value;
+  userInput.value = "";
 
-    // add human message
-    const newHumanSpeechBubble = document.createElement('div')
-    newHumanSpeechBubble.classList.add('speech', 'speech-human')
-    chatbotConversation.appendChild(newHumanSpeechBubble)
-    newHumanSpeechBubble.textContent = question
-    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+  // add human message
+  const newHumanSpeechBubble = document.createElement("div");
+  newHumanSpeechBubble.classList.add("speech", "speech-human");
+  chatbotConversation.appendChild(newHumanSpeechBubble);
+  newHumanSpeechBubble.textContent = question;
+  chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
 
-    // add AI message
-    const newAiSpeechBubble = document.createElement('div')
-    newAiSpeechBubble.classList.add('speech', 'speech-ai')
-    chatbotConversation.appendChild(newAiSpeechBubble)
-    newAiSpeechBubble.textContent = result
-    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+  // add AI message
+  const newAiSpeechBubble = document.createElement("div");
+  newAiSpeechBubble.classList.add("speech", "speech-ai");
+  chatbotConversation.appendChild(newAiSpeechBubble);
+  newAiSpeechBubble.textContent = result;
+  chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
 }
